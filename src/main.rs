7fcbf0418746e682febb177c55d31e9dac0df479
Tesla2000 <fratajczak124@gmail.com>
2025-08-10 12:31:05 +0200

    Adding MCTS methods

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use crate::game_state::{create_initial_game_state, GameState};
+use crate::game_state::create_initial_game_state;
 use crate::moves::all_moves::get_all_moves;
+use crate::moves::move_trait::Move;
 use crate::node::Node;
-use rand::prelude::IndexedRandom;
+use rand::prelude::{IndexedRandom, IteratorRandom};
+use rand::rngs::ThreadRng;
 use rand::{rng, Rng};
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -20,56 +22,95 @@ fn main() {
     let n_players: u8 = 2;
     let mut rng = rng();
 
-    let all_moves = get_all_moves();
-    let mut current_state: GameState = create_initial_game_state(n_players, &mut rng);
+    let root = Node::new(create_initial_game_state(n_players, &mut rng));
+    let valid_moves: Vec<_> = get_all_moves()
+        .into_iter()
+        .filter(|m| m.is_valid(&root.borrow().get_game_state()))
+        .collect();
+    let all_moves: Vec<_> = get_all_moves();
+    for m in &valid_moves {
+        let new_state = m.perform(&root.borrow().get_game_state());
+        roolout(&Node::add_child(&root, new_state), &all_moves, &mut rng);
+    }
+    for _ in 0..100 {
+        let expanded_leaf = get_expanded_leaf(&root, &mut rng);
+        roolout(expanded_leaf, &all_moves, &mut rng)
+    }
+    println!("Finished")
+}
 
-    let mut current = Node::new(current_state.clone());
-
+fn roolout(mut current: &Rc<RefCell<Node>>, all_moves: &Vec<Box<dyn Move>>, rng: &mut ThreadRng){
     loop {
-        let valid_moves: Vec<_> = all_moves
-            .iter()
-            .filter(|m| m.is_valid(&current_state))
-            .collect();
-
-        if valid_moves.is_empty() {
-            println!("No valid moves");
-            backpropagate(&current, 0);
-            break;
-        }
-
-
-        let n_valid_moves = valid_moves.len();
-
-        let chosen_child_index = rng.random_range(0..n_valid_moves);
-
-        let mut new_current_state = current_state.clone();
-        for (index, m) in valid_moves.iter().enumerate() {
-            let new_state = m.perform(&current_state);
-            if index == chosen_child_index {
-                new_current_state = m.perform(&current_state);
-                current = Node::add_child(&current, new_state);
-
-            } else {
-                Node::add_child(&current, new_state);
-            }
-        }
-        current_state = new_current_state;
-        if current_state.get_current_player_index() == 0 {
-            let max_points = current_state.get_players().iter().map(|p| p.get_points()).max().unwrap();
-            if current_state.get_current_player().get_points() == max_points {
-                println!("Player {} wins", current_state.get_current_player_index());
-                backpropagate(&current, 1);
+        if current.borrow().visits == 0 {
+            if !expand_tree(&current, all_moves) {
+                println!("No valid moves");
+                backpropagate(&current, -1.0);
                 break;
             }
+        } else {
+            let expanded_child = get_expanded_leaf(&current, rng);
+            current = expanded_child;
+        }
+
+
+        let new_current;
+        let children_count = current.borrow().children.len();
+        if children_count > 0 {
+            let random_index = rng.random_range(0..children_count);
+            new_current = Rc::clone(&current.borrow().children[random_index]);
+        } else {
+            break;
+        }
+        current = &new_current;
+        if current.borrow().get_game_state().get_current_player_index() == 0 {
+            let max_points = current.borrow().get_game_state().get_players().iter().map(|p| p.get_points()).max().unwrap();
+            if max_points < 15 {
+                continue;
+            }
+            if current.borrow().get_game_state().get_current_player().get_points() == max_points {
+                println!("Player 0 wins");
+                backpropagate(&current, 1.0);
+            } else {
+                println!("Player 0 looses");
+                backpropagate(&current, -1.0);
+            }
+            break;
         }
     }
 }
 
-fn backpropagate(node: &Rc<RefCell<Node>>, is_win: u32) {
+fn expand_tree(current: &Rc<RefCell<Node>>, all_moves: &Vec<Box<dyn Move>>) -> bool {
+    // False if there are no valid moves
+    let valid_moves: Vec<_> = all_moves
+        .iter()
+        .filter(|m| m.is_valid(current.borrow().get_game_state()))
+        .collect();
+
+    for m in &valid_moves {
+        let new_state = m.perform(current.borrow().get_game_state());
+        Node::add_child(current, new_state);
+    }
+    !valid_moves.is_empty()
+}
+
+fn get_expanded_leaf(root_ref: &Rc<RefCell<Node>>, rng: &mut ThreadRng) -> &Rc<RefCell<Node>> {
+    let root = root_ref.borrow();
+    let not_visited_leafs= root.children.iter().filter(|c| c.borrow().visits == 0).collect::<Vec<_>>();
+    if not_visited_leafs.is_empty() {
+        root.children.iter()
+            .max_by(|a, b| Node::ucb1(a).partial_cmp(&Node::ucb1(b)).unwrap())
+            .expect("No children")
+    } else {
+        not_visited_leafs[rng.random_range(0..not_visited_leafs.len())]
+    }
+
+}
+
+fn backpropagate(node: &Rc<RefCell<Node>>, is_win: f32) {
     node.borrow_mut().visits += 1;
     node.borrow_mut().wins += is_win;
     
     if let Some(parent) = node.borrow().parent.upgrade() {
-        backpropagate(&parent, (is_win + 1) % 2);
+        backpropagate(&parent, -is_win);
     }
 }
--- src/node.rs
+++ src/node.rs
@@ -1,10 +1,11 @@
 use std::cell::RefCell;
+use std::process::Child;
 use std::rc::{Rc, Weak};
 use crate::game_state::GameState;
 
 pub struct Node {
     pub game_state: GameState,
-    pub wins: u32,
+    pub wins: f32,
     pub visits: u32,
     pub parent: Weak<RefCell<Node>>,
     pub children: Vec<Rc<RefCell<Node>>>,
@@ -14,7 +15,7 @@ impl Node {
     pub fn new(game_state: GameState) -> Rc<RefCell<Node>> {
         Rc::new(RefCell::new(Node {
             game_state,
-            wins: 0,
+            wins: 0.0,
             visits: 0,
             parent: Weak::new(),
             children: Vec::new(),
@@ -28,7 +29,7 @@ impl Node {
     ) -> Rc<RefCell<Node>> {
         let child = Rc::new(RefCell::new(Node {
             game_state,
-            wins: 0,
+            wins: 0.0,
             visits: 0,
             parent: Rc::downgrade(parent),
             children: Vec::new(),
@@ -39,5 +40,14 @@ impl Node {
 
     pub fn get_game_state(&self) -> &GameState {
         &self.game_state
+    }
+
+    pub fn ucb1(child_ref: &Rc<RefCell<Node>>) -> f32 {
+        let child = child_ref.borrow();
+        if let Some(parent) = child.parent.upgrade() {
+            child.wins / child.visits as f32 + 2.0 * ((parent.borrow().visits as f32).ln() / child.visits as f32).sqrt()
+        } else { 
+            panic!("Parent node not found");
+        }
     }
 }
